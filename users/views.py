b8d9d8d9fe654974698ed5c8564c82952317134b
david-develop <924@holbertonschool.com>
2020-02-23 19:10:32 -0500

    [Add] New post creation feature

--- users/views.py
+++ users/views.py
@@ -16,6 +16,7 @@ from users.models import Profile
 from users.forms import ProfileForm
 
 
+@login_required
 def update_profile(request):
     """Update a user's profile view."""
     profile = request.user.profile
@@ -45,6 +46,7 @@ def update_profile(request):
             'form': form
         }
     )
+
 
 def login_view(request):
     """Login view."""
